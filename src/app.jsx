3a3469748bcd2d27aafcdf75637387f2e15fbd1a
nasser <nasser.elidrissi065@gmail.com>
2016-03-26 15:21:38 +0000

    changes

---
+++ src/app.jsx
@@ -0,0 +1,64 @@
+var React = require('react');
+var ReactFire = require('reactfire');
+var Firebase = require('firebase');
+var Header = require('./header');
+var List = require('./list');
+var rootUrl = 'https://luminous-heat-5304.firebaseio.com/'
+
+var App = React.createClass({
+  mixins: [ ReactFire ],
+  getInitialState: function(){
+    return {
+      items: {},
+      loaded: false
+    }
+  },
+  componentWillMount: function() {
+    this.fb = new Firebase(rootUrl + 'items/');
+    this.bindAsObject(this.fb, 'items');
+    this.fb.on('value', this.handleDataLoaded);
+  },
+  render: function() {
+    return <div className="row panel panel-default">
+      <div className="col-md-8 col-md-offset-2">
+        <h2 className="text-center">
+          To-Do list
+        </h2>
+        <Header itemsStore={this.firebaseRefs.items} />
+        <hr />
+        <div className={"content" + (this.state.loaded ? 'loaded' : '')}>
+          <List items={this.state.items}/>
+          {this.deleteButton()}
+        </div>
+      </div>
+    </div>
+  },
+  deleteButton: function(){
+    if (!this.state.loaded){
+      return
+    }else{
+      return <div className="text-center clear-complete">
+        <hr />
+        <button
+          type="button"
+          onClick={this.onDeleteDoneClicke}
+          className="btn btn-default">
+          Clear Complete
+        </button>
+      </div>
+    }
+  },
+  onDeleteDoneClicke: function () {
+    for(var key in this.state.items){
+      if(this.state.items[key].done === true){
+        this.fb.child(key).remove();
+      }
+    }
+  },
+  handleDataLoaded: function(){
+    this.setState({loaded: true});
+  }
+});
+
+var element = React.createElement(App, {});
+React.render(element, document.querySelector('.container'));
